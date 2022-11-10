e6b3d809a04a6bb6349e0c606b05f22af1fa041e
Dave Fravel <dave@bluegear.io>
2022-11-09 19:37:11 -0500

    wireframes for christmas in cotuit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,148 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="h-full w-screen bg-gray-200 pb-10 pt-6">
+    <div class="max-w-6xl mx-6 sm:mx-auto rounded-lg shadow-md bg-skyBlue">
+      <header class="mx-auto text-center">
+        <img src="./assets/christmas-in-cotuit.png" alt="Christmas in Cotuit" class="w-1/2 mx-auto" />
+        <h1 class="text-xl sm:text-3xl my-4 text-white">Saturday, December 3, 2022</h1>
+      </header>
+
+      <main class="bg-gray-100 p-10 h-full">
+        <div class="grid grid-cols-1 sm:grid-cols-2 gap-y-10 sm:gap-x-10">
+          <div class="col-span-1 rounded-lg shadow-md p-6 bg-white">
+            <h2 class="text-xl text-gray-700">Wednesday, November 30, 4:00pm – 7:00pm</h2>
+            <p class="text-gray-500 mb-3">Food and Diaper Drive to benefit Cotuit Federated Church, Christ the King Food Pantry and Angel House</p>
+            <p class="text-gray-500 mb-3">Drop off your donations at the Fire Station or Freedom Hall</p>
+          </div>
+
+          <div class="col-span-1 rounded-lg shadow-md p-6 bg-white">
+            <h2 class="text-xl text-gray-700">Thursday, December 1, 4:00pm – 7:00pm</h2>
+            <p class="text-gray-500 mb-3">Food and Diaper Drive to benefit Cotuit Federated Church, Christ the King Food Pantry and Angel House</p>
+            <p class="text-gray-500 mb-3">Drop off your donations at the Fire Station or Freedom Hall</p>
+          </div>
+        </div>
+
+        <div class="grid grid-cols-1 mt-10 bg-white rounded-lg shadow-md p-6">
+          <ul class="divide-y divide-gray-200">
+            <li class="py-3">
+              <div class="flex flex-col">
+                <div class="text-gray-600 text-lg">10:00 – 4:00</div>
+                <div class="text-gray-400">
+                  Annual Artisan Craft Show at Freedom Hall featuring 14 artists, pottery, jewelry, Christmas wreaths, shell art, soaps, honey and
+                  more.
+                </div>
+              </div>
+            </li>
+            <li class="py-3">
+              <div class="flex flex-col">
+                <div class="text-gray-600 text-lg">10:00 – 3:00</div>
+                <div class="text-gray-400">
+                  The Cotuit Kettleers Store at Lowell Park will be open! Last chance to purchase great holiday gifts for family & friends
+                </div>
+              </div>
+            </li>
+            <li class="py-3">
+              <div class="flex flex-col">
+                <div class="text-gray-600 text-lg">10:00 - 4:00</div>
+                <div class="text-gray-400">
+                  Historical Society – Visit the Gift Shop for holiday shopping! We have new and unique items for the holidays as well as our great
+                  selection of "Cotuit-wear." Visit www.cotuithistoricalsociety.org for Black Friday and holiday sales.
+                </div>
+              </div>
+            </li>
+            <li class="py-3">
+              <div class="flex flex-col">
+                <div class="text-gray-600 text-lg">10:00-12:00</div>
+                <div class="text-gray-400">
+                  Historical Society. Bring the children to paint a wooden ornament for your tree and enjoy roasted marshmallows and s’mores. Come
+                  early as this is a popular event. Seating is limited.
+                </div>
+              </div>
+            </li>
+            <li class="py-3">
+              <div class="flex flex-col">
+                <div class="text-gray-600 text-lg">12:00-4:00</div>
+                <div class="text-gray-400">Historical Society Guided homestead tours</div>
+              </div>
+            </li>
+            <li class="py-3">
+              <div class="flex flex-col">
+                <div class="text-gray-600 text-lg">10:30-1:30</div>
+                <div class="text-gray-400">Cotuit Library Cookie decorating for all ages</div>
+              </div>
+            </li>
+            <li class="py-3">
+              <div class="flex flex-col">
+                <div class="text-gray-600 text-lg">11:00 – 5:00</div>
+                <div class="text-gray-400">Cotuit Fresh Market Main Street Tastings</div>
+              </div>
+            </li>
+            <li class="py-3">
+              <div class="flex flex-col">
+                <div class="text-gray-600 text-lg">12:00 – 2:00</div>
+                <div class="text-gray-400">Cotuit Fire Station Open House. Chili is back!</div>
+              </div>
+            </li>
+            <li class="py-3">
+              <div class="flex flex-col">
+                <div class="text-gray-600 text-lg">10:00 – 4:00</div>
+                <div class="text-gray-400">Free Admission Cahoon Museum of American Art Winter Open House, family friendly activities</div>
+              </div>
+            </li>
+            <li class="py-3">
+              <div class="flex flex-col">
+                <div class="text-gray-600 text-lg">1:00 – 3:00</div>
+                <div class="text-gray-400">Cahoon Museum Artists' reception for Andrea Moore and Pauline Lim</div>
+              </div>
+            </li>
+            <li class="py-3">
+              <div class="flex flex-col">
+                <div class="text-gray-600 text-lg">1:30</div>
+                <div class="text-gray-400">
+                  At the Cotuit Library Merry Quizmas! Family Holiday Trivia - Form a team to compete with others! Sign up in advance 508-428-8141
+                </div>
+              </div>
+            </li>
+            <li class="py-3">
+              <div class="flex flex-col">
+                <div class="text-gray-600 text-lg">2:30-3:30</div>
+                <div class="text-gray-400">
+                  At the Cotuit Library Reindeer Games - Minute to win it, Holiday edition. 60 second challenges with holiday flair! Hot cocoa on
+                  patio throughout event
+                </div>
+              </div>
+            </li>
+            <li class="py-3">
+              <div class="flex flex-col">
+                <div class="text-gray-600 text-lg">3:30 – 3:45</div>
+                <div class="text-gray-400">At the Town Dock - DJ Lisa Z and Pizza by Zoe Pizza of Mashpee</div>
+              </div>
+            </li>
+            <li class="py-3">
+              <div class="flex flex-col">
+                <div class="text-gray-600 text-lg">3:45</div>
+                <div class="text-gray-400">
+                  Santa’s Arrival by Boat at the Town Dock followed by Tree Lighting and Caroling on theCotuit Village Green: Invocation by Pastor
+                  Angela
+                </div>
+              </div>
+            </li>
+            <li class="py-3">
+              <div class="flex flex-col">
+                <div class="text-gray-600 text-lg">3:00 – 5:00</div>
+                <div class="text-gray-400">Hot Chocolate at the Village Green by Cotuit Fresh Market on the Village.</div>
+              </div>
+            </li>
+          </ul>
+        </div>
+
+        <div class="mt-10">
+          <img src="./assets/ginger-bread.png" class="mx-auto w-full sm:w-1/2" />
+        </div>
+      </main>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
